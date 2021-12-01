340d8d413504200373e6b14733051eaa6616babf
Arief Kalbu <arief.kalbu49@gmail.com>
2021-12-01 11:39:50 +0700

    Path Variables & Query String

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+/*
+Learn From :
+https://www.youtube.com/watch?v=GjI0GSvmcSU&t=4884s
+*/
 package main
 
 import (
@@ -11,6 +15,8 @@ func main() {
 
 	//Routing atau kalau Native GO ini adalah Handler
 	router.GET("/", RootHandler)
+	router.GET("/Product/:id", ProductHandler)       // ini Path Variable
+	router.GET("/ProductQuery", ProductQueryHandler) // ini Query String
 
 	router.Run() //bisa diubah port nya router.Run(":71"), secara default port nya adalah 8080
 }
@@ -19,5 +25,19 @@ func RootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name": "Arief Kalbu",
 		"nim":  "G64154030",
+	})
+}
+
+func ProductHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+	})
+}
+
+func ProductQueryHandler(c *gin.Context) {
+	id := c.Query("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
 	})
 }
